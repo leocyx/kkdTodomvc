08792710fc743fcb38202a239d78053858cba8a4
leocyx <leo8400725@gmail.com>
2022-07-03 15:30:41 +0800

    feat: 製作基本畫面與新增todo功能
    新增項目
    1.添加todo的輸入框與資料顯示列表html
    2.添加todo並將當前list資料儲存到localstorage的功能

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 (function (exports) {
   "use strict";
 
-  var filters = {
+  const filters = {
     all: function (todos) {
       return todos;
     },
@@ -18,11 +18,45 @@
       });
     },
   };
+  function useBasicTodo() {
+    const todos = ref(todoStorage.fetch());
+    const newTodo = ref("");
 
-  const { createApp } = Vue;
+    //若todos發生改變，更新localStorage資料
+    watch(todos.value, (newValue) => {
+      todoStorage.save(newValue);
+    });
+
+    //處理添加todo項目
+    function addTodo() {
+      var value = newTodo.value && newTodo.value.trim();
+      if (!value) {
+        return;
+      }
+      todos.value.push({
+        id: todos.value.length + 1,
+        title: value,
+        completed: false,
+      });
+      newTodo.value = "";
+    }
+
+    return {
+      todos,
+      newTodo,
+      addTodo,
+    };
+  }
+
+  const { createApp, ref, watch } = Vue;
   exports.app = createApp({
     setup() {
-      
-    }
+      const { todos, newTodo, addTodo } = useBasicTodo();
+      return {
+        todos,
+        newTodo,
+        addTodo,
+      };
+    },
   }).mount(".todoapp");
 })(window);
